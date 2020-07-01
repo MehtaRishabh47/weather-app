4f8337c751afc8ed681834c8b7b8d20b1cb90af3
Rishabh <rishabh.mehta98.rm@gmail.com>
2020-07-01 23:34:30 +0530

    Updates

---
+++ precache-manifest.35992f8d2b69c13e8eba1fe42d44ca68.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aab7240770c7f3dd54c1c5beb0ccfa9",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "101e9fa4c1d858377a86",
+    "url": "/weather-app/static/css/2.4467439a.chunk.css"
+  },
+  {
+    "revision": "13279efaf415c5c18e7e",
+    "url": "/weather-app/static/css/main.494fa495.chunk.css"
+  },
+  {
+    "revision": "101e9fa4c1d858377a86",
+    "url": "/weather-app/static/js/2.f508abc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.f508abc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13279efaf415c5c18e7e",
+    "url": "/weather-app/static/js/main.bb795c06.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "80e19d92838adbc8905385af48999139",
+    "url": "/weather-app/static/media/default.80e19d92.jpg"
+  },
+  {
+    "revision": "6141621c97e37a1bd8870834817afffa",
+    "url": "/weather-app/static/media/default_mob.6141621c.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
